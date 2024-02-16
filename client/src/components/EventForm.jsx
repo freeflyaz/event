db5a03331f4d7a4640ff9eb0fe6d8ea2d9032043
Gabe Mata <gabemata@gmail.com>
2024-02-16 11:16:48 +0100

    fet: form css

--- client/src/components/EventForm.jsx
+++ client/src/components/EventForm.jsx
@@ -6,7 +6,26 @@ function EventForm() {
   const [date, setDate] =   useState('');
   const [venue, setVenue] = useState('');
 
-  function onSubmitHandler(e) {
+ async function onSubmitHandler(e) {
+    const topicData = {
+      title: topic
+    }
+    const response = await fetch('http://localhost:300/evets', {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(topicData), // body data type must match "Content-Type" header
+    });
+  
+    if(response.ok) {
+    const res = await response.json(); // created by fetch .json /// must await ///
+      setTopicList([...topicList, res]);
+    }
+  setTopic(''); 
   }
   
   function onTitleChangeHandler (e) {
@@ -23,12 +42,18 @@ function EventForm() {
     <div className="form">
       <h2> Create a new event</h2>
     <form onSubmit={onSubmitHandler}>
-      <label>TITLE</label>
+    <div className="input-group">
+      <label>TITLE<br></br></label>
       <input name="title" value={title} onChange={onTitleChangeHandler} placeholder="Insert a title..." />
-      <label>DATE</label>
+     </div>
+     <div className="input-group">
+      <label>DATE<br></br></label>
       <input name="date" type="datetime-local" value={date} onChange={onDateChangeHandler}   />
-      <label>VENUE</label>
+      </div>
+      <div className="input-group">
+      <label>VENUE<br></br></label>
       <input name="venue" value={venue} onChange={onVenueChangeHandler} placeholder="Insert a venue..." />
+      </div>
       <button>Create</button>
     </form>
   </div>
