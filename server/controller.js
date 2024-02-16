a1842be344de260be7058d9161f9a45c04fcbc9b
Gabe Mata <gabemata@gmail.com>
2024-02-16 09:50:49 +0100

    feat: try catch

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,29 @@
 const model  = require('./model');
 
 const  getEvents = async (req, res) => {
+ try {
     const sendBack = await model.getEvents();
-    res.send(sendBack);
-  };
-
+   res.send(sendBack);
+ } catch (error) {
+    console.error('Error :', error);
+    res.status(500).json({ message: 'Internal server error ' });
+}
+}
   const  postEvents = async (req, res) => {
+try {
     const eventData = req.body;
     const event = await model.postEvents(eventData);
-    res.status(201).json(event);
-  };
+    if (event) {
+        res.status(201).json(event);
+    }
+    else {
+        res.status(400).json(event);
+    }
+} catch (error) {
+    console.error('Error :', error);
+    res.status(500).json({ message: 'Internal server error ' });
+}
+  }
+  
 
   module.exports = {getEvents, postEvents};
