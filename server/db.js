8fbc00bd9303b43d043905922574b74eb3d052c5
Gabe Mata <gabemata@gmail.com>
2024-02-17 12:27:10 +0100

    feat:clean up for deploy + online mongo

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 async function main () {
-  await mongoose.connect('mongodb://localhost:27017/EventDB');
+  await mongoose.connect('mongodb+srv://dbUser:newDbPass@cluster0.knm2nks.mongodb.net/');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
